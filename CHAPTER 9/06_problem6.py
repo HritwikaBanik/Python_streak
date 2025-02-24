89368ba76253d660c3369cda4999361f23d420fa
Hritwika <101196425+HritwikaBanik@users.noreply.github.com>
2025-02-24 12:19:09 +0530

    These are chapter wise practise Sets

--- CHAPTER 9/06_problem6.py
+++
@@ -1,8 +0,0 @@
-file_path = r"C:\Users\91600\Documents\GitHub\Algomagnet\Python_streak\CHAPTER 9\log.html"
-
-with open(file_path , "r") as f:
-    content = f.read()
-    if "python" in content :
-        print("Found")
-    else:
-        print("Not found")
